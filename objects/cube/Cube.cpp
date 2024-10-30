3dfc2b574b5bf9006b3a76f427f9157e1f4d300b
Alessandro Rezzi <alessandrorezzi2000@gmail.com>
2024-10-30 21:19:35 +0100

    refactor: simplify Cube vertices and indices

--- objects/cube/Cube.cpp
+++ objects/cube/Cube.cpp
@@ -9,22 +9,13 @@ Cube::Cube(float size, Vec3f iPos, Vec3f iVel, Vec3f iAxis, float iAngVel,
              theta) {
   this->scale = size;
   std::vector<float> vertices = {
-      1.0f,  1.0f,  -1.0f, 1.0f,  -1.0f, -1.0f, -1.0f, -1.0f, -1.0f,
-      -1.0f, 1.0f,  -1.0f, 1.0f,  1.0f,  1.0f,  1.0f,  -1.0f, 1.0f,
-
-      -1.0f, -1.0f, 1.0f,  -1.0f, 1.0f,  1.0f,  1.0f,  -1.0f, 1.0f,
-      -1.0f, -1.0f, 1.0f,  -1.0f, -1.0f, -1.0f, 1.0f,  -1.0f, -1.0f,
-
-      1.0f,  1.0f,  1.0f,  -1.0f, 1.0f,  1.0f,  -1.0f, 1.0f,  -1.0f,
-      1.0f,  1.0f,  -1.0f, 1.0f,  1.0f,  1.0f,  1.0f,  1.0f,  -1.0f,
-
-      1.0f,  -1.0f, -1.0f, 1.0f,  -1.0f, 1.0f,  -1.0f, 1.0f,  1.0f,
-      -1.0f, 1.0f,  -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f,
+      1.0f,  1.0f,  -1.0f, 1.0f,  -1.0f, -1.0f, -1.0f, -1.0f,
+      -1.0f, -1.0f, 1.0f,  -1.0f, 1.0f,  1.0f,  1.0f,  1.0f,
+      -1.0f, 1.0f,  -1.0f, -1.0f, 1.0f,  -1.0f, 1.0f,  1.0f,
   };
-  std::vector<unsigned int> indices = {
-      0,  1,  2,  0,  2,  3,  4,  5,  6,  4,  6,  7,  8,  9,  10, 8,  10, 11,
-      12, 13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23,
-  };
+  std::vector<unsigned int> indices = {0, 1, 2, 0, 2, 3, 4, 6, 5, 4, 7, 6,
+                                       0, 7, 4, 0, 3, 7, 1, 5, 6, 1, 6, 2,
+                                       0, 4, 1, 5, 1, 4, 7, 3, 2, 7, 2, 6};
   auto attrPointer = []() -> void {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
                           (void *)0);
@@ -41,7 +32,7 @@ void Cube::render(const Camera &camera) const {
 
   Mat4f modelMatrix = mat::translate(position) *
                       mat::scale(Vec3f{scale, scale, scale}) *
-                      mat::rotate(theta, rotationAxis.clone());
+                      mat::rotate(theta, rotationAxis);
   Mat4f pvmMatrix =
       camera.getProjectionMatrix() * camera.getViewMatrix() * modelMatrix;
   program->setUniform("pvmMatrix", pvmMatrix);
