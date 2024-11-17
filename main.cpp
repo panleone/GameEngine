eefc6e6645d2a56f595839842ed871206e20939a
Alessandro Rezzi <alessandrorezzi2000@gmail.com>
2024-11-17 09:28:53 +0100

    feat: Update main to crate a new simple scene

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
+#include <filesystem>
+#include <map>
+#include <cassert>
+
 #include <vector>
+#include <ranges>
 
 #include "WindowManager.h"
-#include "objects/cube/Cube.h"
-#include "objects/cubic_light_source/CubicLight.h"
+#include "objects/Entity.h"
+#include "objects/Light.h"
 #include "Camera.h"
+
+#include "objects/Model.h"
 
 std::unique_ptr<WindowManager> globalWindowManager =
     std::make_unique<WindowManager>(800, 600);
@@ -22,50 +29,145 @@ void processInput(Camera &camera, float deltaTime) {
              deltaTime;
   camera.setCameraPos(speed * dX, 0.0, speed * dZ);
 }
+
+std::string getAbsPath(std::string_view relPath) {
+  std::string currDir = std::filesystem::current_path().string();
+  return std::format("{}/{}", currDir, relPath);
+}
+
+// TODO: refactor in a class ModelLoader once there are more models
+std::map<std::string, Model> loadModels() {
+  std::map<std::string, Model> res;
+  // 1) Backpack model
+  std::string backpackPath = getAbsPath("textures/backpack/backpack.obj");
+  res.insert(std::make_pair("backpack", Model{backpackPath}));
+  // 2) Cube model
+  std::string cubePath = getAbsPath("textures/cube/cube.obj");
+  res.insert(std::make_pair("cube", Model{cubePath}));
+  return res;
+}
+
+std::map<std::string, ShaderProgram> loadShaders() {
+  std::map<std::string, ShaderProgram> res;
+  // 1) Default shader for entities
+  ShaderProgram entityShader("shaders/phong_light_model/phong_light.vs",
+                             "shaders/phong_light_model/phong_light.fs",
+                             "shaders/phong_light_model/phong_light.gs");
+  res.insert(std::make_pair("entity", std::move(entityShader)));
+  // 2) Shader for light sources
+  ShaderProgram lightShader("shaders/light_source/light_source.vs",
+                            "shaders/light_source/light_source.fs");
+  res.insert(std::make_pair("light", std::move(lightShader)));
+
+  // 3) Debug shader for visualizing normal vectors
+  ShaderProgram normalShader("shaders/normal_visualizer/normal_visualizer.vs",
+                             "shaders/normal_visualizer/normal_visualizer.fs",
+                             "shaders/normal_visualizer/normal_visualizer.gs");
+  res.insert(std::make_pair("normal", std::move(normalShader)));
+  return res;
+}
+
+void setUpLight(Light &light) {
+  light.ambientIntensity = light.lightColor * 0.1f;
+  light.diffuseIntensity = light.lightColor * 0.25f;
+  light.specularIntensity = light.lightColor.clone();
+  light.attenuationCoefficients = Vec3f{1.0f, 0.09f, 0.032f};
+}
+
+// TODO: move in another file
 class EntityManager {
   std::vector<Entity *> entities;
   std::vector<Light *> lights;
+  std::map<std::string, ShaderProgram> shaders;
 
 public:
+  EntityManager(std::map<std::string, ShaderProgram> shaders)
+      : shaders{std::move(shaders)} {};
   void addEntity(Entity *entity) { entities.push_back(entity); };
-  void addLight(Light *source) { lights.push_back(source); };
+  void addLight(Light *source) {
+    lights.push_back(source);
+    auto &entityShader = shaders.at("entity");
+    entityShader.use();
+    std::string uniformName = std::format("lights[{}]", lights.size() - 1);
+    entityShader.setUniform(std::format("{}.ambient", uniformName),
+                            source->ambientIntensity);
+    entityShader.setUniform(std::format("{}.diffuse", uniformName),
+                            source->diffuseIntensity);
+    entityShader.setUniform(std::format("{}.specular", uniformName),
+                            source->specularIntensity);
+    entityShader.setUniform(std::format("{}.attenuation", uniformName),
+                            source->attenuationCoefficients);
+    entityShader.setUniform("nLights", lights.size());
+  };
   void update(float deltaTime) {
     for (Entity *entity : entities) {
       entity->update(deltaTime);
     }
-    for (Entity *entity : entities) {
-      entity->handleLights(std::span<Light *>{lights});
+    for (Light *light : lights) {
+      light->update(deltaTime);
+    }
+    // Update light positions in the entity shader
+    auto &entityShader = shaders.at("entity");
+    entityShader.use();
+    for (const auto &[i, light] : std::views::enumerate(lights)) {
+      std::string uniformName = std::format("lights[{}]", i);
+      entityShader.setUniform(std::format("{}.position", uniformName),
+                              light->position);
     }
   }
   void render(const Camera &camera) {
+    auto &entityShader = shaders.at("entity");
+    entityShader.use();
+    auto pvMatrix = camera.getProjectionMatrix() * camera.getViewMatrix();
+    entityShader.setUniform("pvMatrix", pvMatrix);
+    entityShader.setUniform("eyePos", camera.getCameraPos());
     for (const Entity *entity : entities) {
-      entity->render(camera);
+      entityShader.setUniform("mMatrix", entity->modelMatrix());
+      entity->render(entityShader);
+    }
+    auto &lightShader = shaders.at("light");
+    lightShader.use();
+    for (const Light *light : lights) {
+      lightShader.setUniform("lightColor", light->lightColor);
+      lightShader.setUniform("pvmMatrix", camera.getProjectionMatrix() *
+                                              camera.getViewMatrix() *
+                                              light->modelMatrix());
+      light->render(lightShader);
     }
   }
 };
 int main() {
-  EntityManager entityManager;
-  Camera camera{45, Vec3f{0.0f, 0.0f, 8.0f}};
-  Cube cube{
-      1.0f, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}, {1.0f, 0.0f, 0.0f}, 1.0f};
-  CubicLight lightSource1{0.2f, {0.0f, 0.0f, -2.0f}, {0.0f, 0.0f, 0.0f}};
-  CubicLight lightSource2{0.2f, {0.0f, 0.0f, 2.0f}, {0.0f, 0.0f, 0.0f}};
-  CubicLight lightSource3{0.2f, {2.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}};
-  CubicLight lightSource4{0.2f, {-2.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}};
+  EntityManager entityManager{loadShaders()};
+  // Load models and create entities from them
+  auto models = loadModels();
 
-  lightSource2.setColor({0.0f, 1.0f, 0.0f});
-  lightSource3.setColor({1.0f, 0.0f, 0.0f});
-  lightSource4.setColor({0.0f, 0.0f, 1.0f});
+  Entity backpack(models.at("backpack"));
+  backpack.rotationAxis = Vec3f(0.0f, 0.0f, 1.0f);
+  backpack.angularVelocity = 1.0f;
+  entityManager.addEntity(&backpack);
 
-  entityManager.addEntity(&cube);
-  entityManager.addEntity(&lightSource1);
-  entityManager.addLight(&lightSource1);
-  entityManager.addEntity(&lightSource2);
-  entityManager.addLight(&lightSource2);
-  entityManager.addEntity(&lightSource3);
-  entityManager.addLight(&lightSource3);
-  entityManager.addEntity(&lightSource4);
-  entityManager.addLight(&lightSource4);
+  Light light{models.at("cube"), Vec3f(1.0f, 0.0f, 0.0f)};
+  light.position(0) = -4.0f;
+  light.scale = 0.2f;
+  setUpLight(light);
+  Light light2{models.at("cube"), Vec3f(0.0f, 1.0f, 0.0f)};
+  light2.position(0) = 4.0f;
+  light2.scale = 0.2f;
+  setUpLight(light2);
+  Light light3{models.at("cube"), Vec3f(1.0f, 1.0f, 1.0f)};
+  light3.position(1) = 4.0f;
+  light3.scale = 0.2f;
+  setUpLight(light3);
+  Light light4{models.at("cube"), Vec3f(0.0f, 0.0f, 1.0f)};
+  light4.position(1) = -4.0f;
+  light4.scale = 0.2f;
+  setUpLight(light4);
+  entityManager.addLight(&light);
+  entityManager.addLight(&light2);
+  entityManager.addLight(&light3);
+  entityManager.addLight(&light4);
+
+  Camera camera{45, Vec3f{0.0f, 3.0f, 3.0f}};
 
   globalWindowManager->disableMouseCursor();
   glEnable(GL_DEPTH_TEST);
