4996c13a21745ca71fc8669a8c3c81bc6be31b03
Alessandro Rezzi <alessandrorezzi2000@gmail.com>
2024-11-17 09:11:54 +0100

    feat: Make the texture class un-movable and add enumclass Texture type

--- textures/Texture.cpp
+++ textures/Texture.cpp
@@ -16,7 +16,7 @@ static unsigned int getImageFormat(std::string_view texturePath) {
         std::format("Texture format {} not supported!", texturePath));
   }
 }
-Texture::Texture(std::string_view texturePath) {
+Texture::Texture(std::string_view texturePath, TextureType type) : type{type} {
   int width, height, nrChannels;
   stbi_set_flip_vertically_on_load(true);
   unsigned char *data =
@@ -25,17 +25,14 @@ Texture::Texture(std::string_view texturePath) {
     throw std::runtime_error(
         std::format("Cannot load texture {}!", texturePath));
   }
-  glGenTextures(1, &this->textureId);
-  glBindTexture(GL_TEXTURE_2D, this->textureId);
+  glGenTextures(1, &textureID);
+  glBindTexture(GL_TEXTURE_2D, textureID);
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0,
                getImageFormat(texturePath), GL_UNSIGNED_BYTE, data);
   glGenerateMipmap(GL_TEXTURE_2D);
   stbi_image_free(data);
 }
 
-void Texture::bind(unsigned int textureSlot) const {
-  glActiveTexture(textureSlot);
-  glBindTexture(GL_TEXTURE_2D, this->textureId);
-}
+void Texture::bind() const { glBindTexture(GL_TEXTURE_2D, textureID); }
 
-Texture::~Texture() { glDeleteBuffers(1, &textureId); }
+Texture::~Texture() { glDeleteBuffers(1, &textureID); }
--- textures/Texture.h
+++ textures/Texture.h
@@ -3,14 +3,23 @@
 
 #include <string_view>
 
+enum class TextureType { DIFFUSE, SPECULAR };
+
+/**
+ * This class represents a texture. It's neither movable nor copyable,
+ * wrap it inside a shared_ptr to make multiple Models with the same texture
+ */
 class Texture {
-  unsigned int textureId;
+private:
+  unsigned int textureID;
+  TextureType type;
 
 public:
-  Texture(std::string_view texturePath);
+  Texture(std::string_view texturePath, TextureType type);
   Texture(const Texture &texture) = delete;
-  Texture(Texture &&texture) = default;
-  void bind(unsigned int textureSlot) const;
+  Texture(Texture &&texture) = delete;
+  void bind() const;
+  TextureType getType() const { return type; };
   ~Texture();
 };
 
