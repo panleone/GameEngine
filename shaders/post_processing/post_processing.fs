70cce540222b3a81d6f69fe1c2180c794c5344c5
Alessandro Rezzi <alessandrorezzi2000@gmail.com>
2024-11-21 14:56:55 +0100

    refactor: Perform gamma correction in the post processing stage

--- shaders/post_processing/post_processing.fs
+++ shaders/post_processing/post_processing.fs
@@ -8,7 +8,17 @@ in VS_OUT {
     vec2 TexCoord;
 } fs_in;
 
+float Convert_sRGB_FromLinear (float theLinearValue) {
+  return theLinearValue <= 0.0031308
+       ? theLinearValue * 12.92
+       : pow (theLinearValue, 1.0/2.4) * 1.055 - 0.055;
+}
 void main()
 {
-    FragColor = texture(postProcessing, fs_in.TexCoord);
+    float gamma = 2.2;
+    vec4 texCol = texture(postProcessing, fs_in.TexCoord);
+    FragColor.r = Convert_sRGB_FromLinear(texCol.x);
+    FragColor.g = Convert_sRGB_FromLinear(texCol.y);
+    FragColor.b = Convert_sRGB_FromLinear(texCol.z);
+    FragColor.w = texCol.w;
 }
